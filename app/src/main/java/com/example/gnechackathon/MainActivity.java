3af1b3dcaf0c7c26033aa80544a9ad1f9f349689
Breanna12 <56575941+Breanna12@users.noreply.github.com>
2023-07-29 22:22:42 -0700

    created form layout

--- app/src/main/java/com/example/gnechackathon/MainActivity.java
+++ app/src/main/java/com/example/gnechackathon/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.gnechackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button childHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        childHome = (Button) findViewById(R.id.ChildButton);
+        childHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openChildHome();
+            }
+        });
+    }
+
+    public void openChildHome() {
+        Intent intent = new Intent(this, child_home.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/gnechackathon/child_home.java
+++ app/src/main/java/com/example/gnechackathon/child_home.java
@@ -2,13 +2,31 @@ package com.example.gnechackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class child_home extends AppCompatActivity {
+
+    private Button formButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_home);
+
+        formButton = (Button) findViewById(R.id.formButton);
+        formButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openForm();
+            }
+        });
+    }
+
+    public void openForm() {
+        Intent intent = new Intent(this, form.class);
+        startActivity(intent);
     }
 }
