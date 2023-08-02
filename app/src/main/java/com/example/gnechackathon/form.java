6c6a520c541ae600e8c518c95af48aa0fd86b256
Breanna12 <56575941+Breanna12@users.noreply.github.com>
2023-08-01 23:47:42 -0700

    added file upload to form

--- app/src/main/java/com/example/gnechackathon/form.java
+++ app/src/main/java/com/example/gnechackathon/form.java
@@ -1,8 +1,16 @@
 package com.example.gnechackathon;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class form extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+    }
+
+    ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Uri uri = data.getData();
+
+
+
+                    }
+                }
+            }
+    );
+
+    public void openFileDialog(View view) {
+        Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        data.setType("*/*");
+        data = Intent.createChooser(data, "Choose a file");
+        sActivityResultLauncher.launch(data);
     }
 }
